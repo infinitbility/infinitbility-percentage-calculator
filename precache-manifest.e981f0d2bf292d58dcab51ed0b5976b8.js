41b21eeb24dad40fb7a5fffbfdbf7ef7e03dadf8
lorebook <contactshanitiwari@gmail.com>
2020-04-07 10:31:06 +0530

    Updates

---
+++ precache-manifest.e981f0d2bf292d58dcab51ed0b5976b8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0eaabbb697e31ec6b8605d8d3b1fb0aa",
+    "url": "/infinitbility-percentage-calculator/index.html"
+  },
+  {
+    "revision": "223dad11f675684e8f9a",
+    "url": "/infinitbility-percentage-calculator/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "62c409df3ed334e48f78",
+    "url": "/infinitbility-percentage-calculator/static/js/2.34eed2e9.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/infinitbility-percentage-calculator/static/js/2.34eed2e9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "223dad11f675684e8f9a",
+    "url": "/infinitbility-percentage-calculator/static/js/main.e733aea7.chunk.js"
+  },
+  {
+    "revision": "584aae5e159722ce123b",
+    "url": "/infinitbility-percentage-calculator/static/js/runtime-main.e129e8fb.js"
+  }
+]);
